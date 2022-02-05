1021792add02d899789d1a892544f2fd92148bbc
rizki wahyudie git <rizkiwahyudie@gmail.com>
2022-02-05 16:36:49 +0700

    add file

---
+++ tugas8_classObject.js
@@ -0,0 +1,10 @@
+function property(){
+  var sepakbola = {
+    nama : 'Messi',
+    umur : 34,
+    klub : 'Paris Saint-Germain',
+    mahar : 'Free Transfer',
+  }
+  console.log(sepakbola.nama)
+}
+property();
